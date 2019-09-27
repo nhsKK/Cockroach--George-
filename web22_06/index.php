dfcca8ddc8adcefbd66789ebdaeb1c61ae6cd43e
nhsKK <50687024+nhsKK@users.noreply.github.com>
2019-09-27 18:31:14 +0900

    Add files via upload

---
+++ web22_06/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://strangenerdlife-css.s3-ap-northeast-1.amazonaws.com/common.css">
+    <title>StrangeNerdLife</title>
+</head>
+<body>
+    
+    <!-- html読み込み -->
+    <?php
+    include("html/header.html");
+    include("html/index.html");
+    include("html/footer.html");
+    ?>
+    
+    
+</body>
+
+</html>
